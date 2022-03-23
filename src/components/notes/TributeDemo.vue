75dbf8b29989eaee08b2c87ca506f35985c75a90
peterroe <peterroe@163.com>
2022-03-23 12:35:35 +0800

    update

---
+++ src/components/notes/TributeDemo.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Tribute from "tributejs";
+import 'Tributejs/dist/tribute.css'
+
+var tribute = new Tribute({
+  values: [
+    { key: "Phil Heartman", value: "pheartman" },
+    { key: "Gordon Ramsey", value: "gramsey" }
+  ]
+});
+ onMounted(() => {
+   tribute.attach(document.querySelector("#tributeNode"));
+ })
+</script>
+
+<template>
+  <div>
+    <input
+      type="text"
+      id="tributeNode"
+      cols="30"
+      rows="10"
+      class="border resize-none"
+    />
+  </div>
+</template>
