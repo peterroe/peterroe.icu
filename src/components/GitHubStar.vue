1dd33cd1e32463fca9cf5f354d0536b486f37201
peterroe <peterroe@163.com>
2022-07-23 14:32:40 +0800

    fix: title style

--- src/components/GitHubStar.vue
+++ src/components/GitHubStar.vue
@@ -3,5 +3,11 @@ defineProps<{ repo: string }>()
 </script>
 
 <template>
-  <img :src="`https://badgen.net/github/stars/${repo}?icon=github&color=4ab8a1`" />
+  <img :src="`https://badgen.net/github/stars/${repo}?icon=github&color=4ab8a1`">
 </template>
+
+<style scoped>
+img {
+  margin-right: 1px;
+}
+</style>
